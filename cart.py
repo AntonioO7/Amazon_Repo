8262e0d5ff164226cebcbda0855b8c49b5ef4721
Sumeet <sumeetmistry34@gmail.com>
2022-03-30 08:35:13 +0530

    Commit1

--- cart.py
+++ cart.py
@@ -1,1 +1,3 @@
 Your cart is full
+Please place another order to proceed further
+
--- login.py
+++ login.py
@@ -1,1 +1,2 @@
 Hey this is the Login page
+You can browse thing you like here and place order for yourself and your loved once.
